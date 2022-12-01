1d3e87021ec7280f9a8c741a39201dde34dc832f
Yuliamsyah <yuliamsyah1@gmail.com>
2022-12-02 04:58:33 +0700

    membuat model untuk tabel aa_user

---
+++ app/Models/aa_userModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Aa_userModel extends Model
+{
+    protected $table = 'aa_user'; //daftarkan nama tabel disini
+    protected $primaryKey = 'id_user'; //daftarkan field primari key disini
+}
