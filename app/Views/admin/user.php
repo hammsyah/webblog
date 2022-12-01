5e0e3afd2e71dc7c8573e9b5045d471d6d577aa8
hammsyah <yuliamsyah1@gmail.com>
2022-12-01 12:22:26 +0700

    edit navbar dan view user

--- app/Views/admin/user.php
+++ app/Views/admin/user.php
@@ -3,14 +3,27 @@
 
 <div class="container">
 
-    <div class="row mt-3">
-        <div class="col-auto mr-auto">
+    <div class="row mt-7">
+        <div class="col-sm-6 mr-auto">
             <h3>DAFTAR USER</h3>
         </div>
-        <div class="col-auto">.
-            <a href="" class="btn btn-success btn-sm text-uppercase fw-bold" type="submit">Tambah User</a>
+        <div class="col-sm-4">
+
+            <!-- <form class="form-inline my-2 my-lg-0"> -->
+            <form class="form-inline ml-5">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success " type="submit">Search</button>
+            </form>
+
+        </div>
+        <div class="col-sm-2">
+            <button type="button" class="btn btn-success btn-sm text-uppercase fw-bold ml-5" data-toggle="modal" data-target="#userModal">
+                Tambah User
+            </button>
+
         </div>
     </div>
+
 
     <div class="row">
         <div class="col mt-2">
@@ -27,21 +40,60 @@
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
+                        <td>Sinta Prameswari</td>
+                        <td>sinta@weblog.com</td>
+                        <td>Admin</td>
+                        <td>
+                            <a href="" class="btn btn-success btn-sm text-uppercase fw-bold" type="submit">edit</a>
+                            <a href="" class="btn btn-danger btn-sm text-uppercase fw-bold" type="submit">Hapus</a>
+                        </td>
                     </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@mdo</td>
-                    </tr>
+
 
                 </tbody>
             </table>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">TAMBAH USER</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="<?php echo base_url() . 'mahasiswa/tambah_aksi'; ?> ">
+
+                    <div class="form-group">
+                        <label>Real Name</label>
+                        <input type="text" name="realname" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>User Name</label>
+                        <input type="text" name="username" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Pasword</label>
+                        <input type="password" name="pasword" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Role</label>
+                        <input type="text" name="role" class="form-control">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">BATAL</button>
+                <button type="button" class="btn btn-primary">SIMPAN</button>
+            </div>
         </div>
     </div>
 </div>
