99a06b9533a14ebd558cb2bf10f5dfd64f07084a
Yuliamsyah <yuliamsyah1@gmail.com>
2022-12-02 05:29:36 +0700

    Komentar keterangan beberapa skrip

--- app/Views/admin/user.php
+++ app/Views/admin/user.php
@@ -1,15 +1,17 @@
+<!-- panggil header disini -->
 <?= $this->include('layout/header'); ?>
+<!-- panggil navbar admin disini -->
 <?= $this->include('layout/navbar_admin'); ?>
 
 <div class="container">
-
+    <!-- membuat row atau baris dibawah navbar -->
+    <!-- bootstrap membagi kolom dalam layar menjadi 12 kolom. tinggal kita bagi dengan class col-sm-xx, xx jumlah kolom -->
+    <!-- misal 1 baris kita bagi 2 kolom sama besar, col-sm-6 dan col-sm-6 jadi total tetap 12 colom -->
     <div class="row mt-3">
         <div class="col-sm-6 mr-auto">
             <h4>DAFTAR USER</h4>
         </div>
         <div class="col-sm-4">
-
-            <!-- <form class="form-inline my-2 my-lg-0"> -->
             <form class="form-inline ml-5">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success " type="submit">Search</button>
@@ -17,6 +19,7 @@
 
         </div>
         <div class="col-sm-2">
+            <!-- tombol triger untuk modal -->
             <button type="button" class="btn btn-success btn-sm text-uppercase fw-bold ml-5" data-toggle="modal" data-target="#userModal">
                 Tambah User
             </button>
@@ -24,11 +27,13 @@
         </div>
     </div>
 
-
+    <!-- membuat tabel disini -->
     <div class="row">
         <div class="col mt-2">
+            <!-- mt-2 -> margin top  -->
             <table class="table table-hover">
                 <thead>
+                    <!-- header tabel -->
                     <tr>
                         <th scope="col">#</th>
                         <th scope="col">Real Name</th>
@@ -38,6 +43,7 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <!-- body tabel, isi data kita ambil dari database yang sudah diatur oleh controller user -->
                     <?php $no = 1; ?>
                     <?php foreach ($user as $u) : ?>
                         <tr>
@@ -51,8 +57,6 @@
                             </td>
                         </tr>
                     <?php endforeach; ?>
-
-
                 </tbody>
             </table>
         </div>
@@ -60,7 +64,8 @@
 </div>
 
 
-<!-- Modal -->
+<!-- Modal, skrip ini untuk membuat pop out dengan menekan tombol  triger -->
+<!-- kita contek dari dokumentasi bootstrap 4 -->
 <div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
