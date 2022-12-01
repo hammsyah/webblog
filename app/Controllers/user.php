9975e006cd52fd06dbdd12553af5e0950006e1cd
Yuliamsyah <yuliamsyah1@gmail.com>
2022-12-02 04:58:10 +0700

    tambah tampilkan data user dari database

--- app/Controllers/user.php
+++ app/Controllers/user.php
@@ -2,10 +2,26 @@
 
 namespace App\Controllers;
 
+use App\Models\Aa_userModel;
+
 class User extends BaseController
 {
+    protected $aa_userModel; //buat properti baru agar bisa dipakai di semua method
+
+    public function __construct() //kita bikin untuk menjalankan skrip yang dipakai untuk semua method disini
+    {
+        $this->aa_userModel = new Aa_userModel(); //membuat objecb baru bernama $aa_userModel
+    }
     public function index()
     {
-        return view('admin/user');
+
+        $user = $this->aa_userModel->findAll(); //membuat variabel user dan diisi dari data tabel user database
+
+        $data = [ //membuat variabel data untuk dikirim ke view user
+            'title' => 'Daftar User',
+            'user' => $user
+        ];
+
+        return view('admin/user', $data); //kirim $data ke view user
     }
 }
