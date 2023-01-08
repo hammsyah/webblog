1856e66e3681c777227f77bb8388318938deebaa
Yuliamsyah <yuliamsyah1@gmail.com>
2023-01-08 13:12:53 +0700

    update DB

--- analisis/db_webblog.sql
+++ analisis/db_webblog.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.10
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 13, 2022 at 11:54 AM
+-- Generation Time: Jan 08, 2023 at 01:08 PM
 -- Server version: 8.0.31
--- PHP Version: 7.4.33
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,11 +30,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `aa_user` (
   `id_user` int NOT NULL,
   `fid_level` int NOT NULL,
-  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `pasword` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `realname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `enable_user` tinyint(1) NOT NULL,
-  `keterangan` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `user_name` varchar(50) NOT NULL,
+  `pasword` varchar(100) NOT NULL,
+  `realname` varchar(100) NOT NULL,
+  `enable_user` tinyint(1) DEFAULT NULL,
+  `keterangan` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -43,13 +42,14 @@ CREATE TABLE `aa_user` (
 --
 
 INSERT INTO `aa_user` (`id_user`, `fid_level`, `user_name`, `pasword`, `realname`, `enable_user`, `keterangan`) VALUES
-(1, 1, 'budi@webblog.com', '123', 'Anggara Budi Santoso', 1, 'Super Admin'),
+(1, 1, 'budi@webblog.com', '$2y$10$GAY6UmXbDqxHGjwvqPsgsOYaHyb.8nXsP5CqfbuRl4JyRuhp0Ey5K', 'Anggara Budi Santoso', 1, 'Super Admin'),
 (2, 2, 'datuk@webblog.com', '1234', 'Datuk Maringgi', 1, 'penulis'),
-(3, 3, 'tejo@webblog.com', '1234', 'Tejo Sunaryo', 1, ''),
-(4, 2, 'jarwo@webblog.com', '6789', 'Jarwo Agung', 1, ''),
-(5, 3, 'bejo@webblog.com', '1234', 'Bejo Santoso', 1, 'test alert update'),
-(7, 3, 'santosa@webblog.com', '1234', 'SANTOSA AJI', 1, ''),
-(13, 3, 'afdsfa@afaf.afasf', '123456', 'xxsa', 0, 'wwwwww');
+(3, 2, 'bondan@webblog.com', '1234', 'Bondan Sendiko', 1, 'Operator'),
+(6, 3, 'bagong@webblog.com', '1234', 'Bagong Pambudi Rojo', 1, NULL),
+(8, 1, 'raden@gmail.com', '1234', 'Raden Joko', 1, NULL),
+(9, 2, 'dalijo@webblog.com', '1234', 'Dalijo', 1, ''),
+(11, 2, 'asfasfd@gsg.ffaf', 'zvcxcvz', 'adsfas', 0, 'testing'),
+(14, 1, 'prayogi@webblog.com', '$2y$10$vBO/iqXVxgr0aXfuJTM6V.CCsdviVcEkCGY8A5pRRYX8ArC0ncobe', 'prayogi', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -59,7 +59,7 @@ INSERT INTO `aa_user` (`id_user`, `fid_level`, `user_name`, `pasword`, `realname
 
 CREATE TABLE `aa_user_level` (
   `id` int NOT NULL,
-  `nama_level` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_level` varchar(20) NOT NULL,
   `id_level` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -69,8 +69,8 @@ CREATE TABLE `aa_user_level` (
 
 INSERT INTO `aa_user_level` (`id`, `nama_level`, `id_level`) VALUES
 (1, 'admin', 1),
-(2, 'Operator', 2),
-(3, 'Penulis', 3);
+(5, 'Operator', 2),
+(7, 'Penulis', 3);
 
 -- --------------------------------------------------------
 
@@ -80,7 +80,7 @@ INSERT INTO `aa_user_level` (`id`, `nama_level`, `id_level`) VALUES
 
 CREATE TABLE `dm_category` (
   `id_category` int NOT NULL,
-  `nama_category` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+  `nama_category` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -92,9 +92,9 @@ CREATE TABLE `dm_category` (
 CREATE TABLE `tr_article` (
   `id_article` int NOT NULL,
   `fid_catergory` int NOT NULL,
-  `title_article` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `content_article` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `img_article` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `title_article` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `content_article` text COLLATE utf8mb4_general_ci NOT NULL,
+  `img_article` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `soft_delete` tinyint(1) DEFAULT NULL,
   `show` tinyint(1) DEFAULT NULL,
   `enable_comm` tinyint(1) DEFAULT NULL,
@@ -139,13 +139,13 @@ ALTER TABLE `tr_article`
 -- AUTO_INCREMENT for table `aa_user`
 --
 ALTER TABLE `aa_user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `aa_user_level`
 --
 ALTER TABLE `aa_user_level`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `dm_category`
